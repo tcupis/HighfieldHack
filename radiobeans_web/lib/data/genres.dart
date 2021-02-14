43d45d00f254e5d564da0d88f18c5bad2fdb9994
Dan Berry <danb@lumico.io>
2021-02-14 01:18:18 +0000

    Added play button and stuff

--- radiobeans_web/lib/data/genres.dart
+++ radiobeans_web/lib/data/genres.dart
@@ -1,9 +1,24 @@
-List<String> pop = [
-  "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/pop%2FBabyJake%20-%20Confidant.mp3?alt=media&token=927e81b8-a2e2-44d8-8575-4618147f3774",
-  "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/pop%2FBakar%20-%20Stop%20Selling%20Her%20Drugs%20(feat%20Dominic%20Fike).mp3?alt=media&token=f963d8cd-1496-4fc4-a35c-aa00bec2863a",
-  "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/pop%2FCalvin%20Harris%20-%20Slide%20(feat%20Frank%20Ocean%20%26%20Migos).mp3?alt=media&token=514a2378-be90-42ad-99f0-25c26e804dae",
-  "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/pop%2FDominic%20Fike%20-%20Cancel%20Me.mp3?alt=media&token=19f800d1-df0f-4e73-bc6c-ec7f86a16727",
-  "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/pop%2FFrank%20Ocean%20-%20Pink%20%20White.mp3?alt=media&token=dccbc2e7-a2c9-43ae-ae4d-cdb956e39d3a",
-  "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/pop%2FLoyle%20Carner%20-%20Ottolenghi.mp3?alt=media&token=9fd8a8c7-15d3-4a58-a23f-c8c6cc1bbd3c",
-  "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/pop%2FMac%20Miller%20-%20Whats%20the%20Use.mp3?alt=media&token=a75a2ba4-d2a0-44fd-ab46-8e1b368b023d"
+import 'package:radiobeans_web/models/song.dart';
+
+List<Song> jungle = [
+  Song(
+    url: "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/jungle%2FAlex%20Reece%20-%20Pulp%20Fiction.mp3?alt=media&token=57e24dbe-94b8-4f9d-b0a6-ed4b210964dc",
+    artist: "Alex Reece",
+    title: "Pulp Fiction"
+  ),
+  Song(
+    url: "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/jungle%2FDJ%20Die%20-%20Play%20It%20for%20Me.mp3?alt=media&token=2577587d-523e-403a-816a-bd7a1e4f2f75",
+    artist: "DJ Die",
+    title: "Play It for Me"
+  ),
+  Song(
+    url: "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/jungle%2FDJ%20Hype%20-%20Peace%20Love%20and%20Unity.mp3?alt=media&token=54b71e5a-45cb-4b60-bff8-025d67790d73",
+    artist: "DJ Hype",
+    title: "Peace Love and Unity"
+  ),
+  Song(
+    url: "https://firebasestorage.googleapis.com/v0/b/radiobeans-f4d02.appspot.com/o/jungle%2FDillinja%20-%20Sovereign%20Melody.mp3?alt=media&token=4ac2a4c4-c5f5-4488-8fab-2e5dcc4da2dc",
+    artist: "Dillinja",
+    title: "Sovereign Melody"
+  ),
 ];
--- radiobeans_web/lib/screens/session_screen.dart
+++ radiobeans_web/lib/screens/session_screen.dart
@@ -1,12 +1,14 @@
 import 'dart:typed_data';
-
+import 'package:uuid/uuid.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:radiobeans_web/data/genres.dart';
 import 'package:radiobeans_web/widgets/logo.dart';
 import 'package:firebase/firebase.dart';
 import '../theme_data.dart';
 import 'package:dart_tags/dart_tags.dart';
 import 'package:http/http.dart';
+import 'dart:math';
 
 class SessionScreen extends StatefulWidget {
   @override
@@ -15,62 +17,135 @@ class SessionScreen extends StatefulWidget {
 
 class _SessionScreenState extends State<SessionScreen> {
   final player = AudioPlayer();
+  String genre = "jungle";
+  List<int> chosen = [];
   String title = "";
   String artist = "";
+  String sessionId = Uuid().v1();
 
   @override
   void initState() {
     super.initState();
-
     Database db = database();
-    DatabaseReference ref = db.ref('157629');
-
+    DatabaseReference ref = db.ref('sessions/$sessionId');
+    buildRandomSession();
     ref.onValue.listen((e) {
       DataSnapshot datasnapshot = e.snapshot;
-      initAudio(datasnapshot.toJson()['playing_now']);
+      // initAudio(datasnapshot.toJson()['playing_now']);
       // Do something with datasnapshot
+      title = datasnapshot.toJson()['title'];
+      artist = datasnapshot.toJson()['artist'];
+      initAudio(datasnapshot.toJson()['url']);
     });
+  }
+
+  void buildRandomSession() async {
+    // Select a random starting song
+    var rng = new Random();
+    var numOptions = 2;
+    var nowPlaying = rng.nextInt(jungle.length);
+    chosen.add(nowPlaying);
+    for (int i = 0; i < numOptions; i++) {
+      var songIndex = rng.nextInt(jungle.length);
+      while (chosen.contains(songIndex)) {
+        songIndex = rng.nextInt(jungle.length);
+      }
+      chosen.add(songIndex);
+    }
+
+    var songData = {
+      "url": jungle[chosen[0]].url,
+      "artist": jungle[chosen[0]].artist,
+      "title": jungle[chosen[0]].title,
+      "voting_options": [
+        {
+          "url": jungle[chosen[1]].url,
+          "artist": jungle[chosen[1]].artist,
+          "title": jungle[chosen[1]].title,
+          "votes": 0
+        },
+        {
+          "url": jungle[chosen[2]].url,
+          "artist": jungle[chosen[2]].artist,
+          "title": jungle[chosen[2]].title,
+          "votes": 0
+        }
+      ]
+    };
+    database().ref('sessions/' + sessionId).set(songData);
   }
 
   void initAudio(String songUrl) async {
     var duration = await player.setUrl(songUrl);
     player.play();
-    getMetaData(songUrl);
-  }
-
-  void getMetaData(String songUrl) async {
-    Response response = await get(songUrl);
-    TagProcessor tp = new TagProcessor();
-    var l = await tp.getTagsFromByteData(
-        response.bodyBytes.buffer.asByteData(), [TagType.id3v2]);
-    setState(() {
-      title = l[0].tags["title"];
-      artist = l[0].tags['artist'];
-    });
-    print(l[0].tags['picture']['Cover (front)']);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: Colors.black, //change your color here
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage("images/beans_bg.png")),
         ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Logo(),
-      ),
-      body: Column(
-        children: [
-          Container(),
-          Text("Session ID: 157629"),
-          Text(
-            title,
-            style: strongText,
-          ),
-          Text(artist)
-        ],
+        child: FutureBuilder(
+          builder: (context, snapshot) {
+            return Column(
+              children: [
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Logo(),
+                    )),
+                Container(),
+                Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Text(sessionId)),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  title,
+                  style: strongText,
+                ),
+                Text(artist),
+                SizedBox(
+                  height: 25,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (player.playing) {
+                      setState(() {
+                        player.pause();
+                      });
+                    } else {
+                      setState(() {
+                        player.play();
+                      });
+                    }
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: player.playing
+                            ? Colors.redAccent
+                            : Colors.lightGreen,
+                        border: Border.all(color: Colors.black, width: 2),
+                        borderRadius: BorderRadius.circular(12)),
+                    padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                    child: player.playing
+                        ? Icon(Icons.pause)
+                        : Icon(Icons.play_arrow),
+                  ),
+                )
+              ],
+            );
+          },
+        ),
       ),
     );
   }
